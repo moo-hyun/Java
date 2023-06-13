9242b13680ca6961461f17a7ce3770d18d1173e5
vkfksvoeld <vkfksvoeld@gmail.com>
2023-06-14 00:30:55 +0900

    테스트 북 추가

--- TestBook/src/ch04/Start.java
+++ TestBook/src/ch04/Start.java
@@ -1,5 +1,22 @@
 package ch04;
-
+/**
+ * 날짜 : 2023/6/13
+ * 이름 : 김무현
+ * 내용 : 별 만들어보기 
+ *
+ */
 public class Start {
+	
+	public static void main(String[] args) {
+		
+		for(int start = 0; start <=5; start++) {
+			
+			for(int end=1 ; end <= start ; end++)
+				
+				System.out.print("★");
+				System.out.println();
+		}
+		
+	}
 
 }
