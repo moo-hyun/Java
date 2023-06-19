6b9cb13ab4b1bb7219e02d020be76dd202cd97cb
vkfksvoeld <vkfksvoeld@gmail.com>
2023-06-19 12:37:43 +0900

    테스트북 ch05추바

---
+++ TestBook/src/ch05/CarExample.java
@@ -0,0 +1,19 @@
+package ch05;
+
+public class CarExample {
+	public static void main(String[] args) {
+		
+		Car myCar =new Car();
+		
+		myCar.setGas(0);
+		if(myCar.isLeftGas()) {
+			System.out.println("출발합니다.");
+		
+		
+		myCar.run();
+		
+		}
+		System.out.println("gas를 주입하세요.");
+	}
+
+}
