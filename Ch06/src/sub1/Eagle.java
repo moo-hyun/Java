e0872d76323df6f5cae8487ef0e60fb8e4710259
vkfksvoeld <vkfksvoeld@gmail.com>
2023-06-26 12:48:09 +0900

    수업내용추가

---
+++ Ch06/src/sub1/Eagle.java
@@ -0,0 +1,14 @@
+package sub1;
+
+public class Eagle extends Animal{
+	
+	@Override
+	public void move() {
+		System.out.println("Eagle fiy...");
+		
+	}
+	@Override
+	public void hunt() {
+		System.out.println("Ealge hunt...");
+	}
+}
